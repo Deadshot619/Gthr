2d8dd6445ab8153619302649768df5cba4110a50
Pankaj Makhija <pankaj.makhija@bank-abc.com>
2021-06-28 20:25:14 +0530

    - Add Timber logging library

--- app/src/main/java/com/gthr/gthrcollect/GthrCollect.kt
+++ app/src/main/java/com/gthr/gthrcollect/GthrCollect.kt
@@ -1,6 +1,18 @@
 package com.gthr.gthrcollect
 
 import android.app.Application
+import timber.log.Timber
 
 class GthrCollect: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initLogger()
+    }
+
+    private fun initLogger() {
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
+    }
 }
---
+++ app/src/main/java/com/gthr/gthrcollect/utils/logger/GthrLogger.kt
@@ -0,0 +1,27 @@
+package com.gthr.gthrcollect.utils.logger
+
+import timber.log.Timber
+
+object GthrLogger {
+    private const val TAG = "GTHR_Application"
+
+    fun i(tag: String = TAG, message: String) {
+        Timber.tag(tag).i(message)
+    }
+
+    fun d(tag: String = TAG, message: String) {
+        Timber.tag(tag).d(message)
+    }
+
+    fun v(tag: String = TAG, message: String) {
+        Timber.tag(tag).v(message)
+    }
+
+    fun e(tag: String = TAG, message: String) {
+        Timber.tag(tag).e(message)
+    }
+
+    fun w(tag: String = TAG, message: String) {
+        Timber.tag(tag).w(message)
+    }
+}
