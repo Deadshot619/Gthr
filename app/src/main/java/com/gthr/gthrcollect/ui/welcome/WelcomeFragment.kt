ae0616917be2f2d5c72402f8ef36370ed6b37790
Pankaj Makhija <pankaj.makhija@bank-abc.com>
2021-07-02 17:38:45 +0530

    - Complete Welcome Screen

---
+++ app/src/main/java/com/gthr/gthrcollect/ui/welcome/WelcomeFragment.kt
@@ -0,0 +1,30 @@
+package com.gthr.gthrcollect.ui.welcome
+
+import androidx.fragment.app.viewModels
+import com.gthr.gthrcollect.databinding.WelcomeFragmentBinding
+import com.gthr.gthrcollect.ui.base.BaseFragment
+import com.gthr.gthrcollect.utils.customviews.CustomAuthenticationButton
+
+class WelcomeFragment : BaseFragment<WelcomeViewModel, WelcomeFragmentBinding>() {
+    override val mViewModel: WelcomeViewModel by viewModels()
+    override fun getViewBinding() = WelcomeFragmentBinding.inflate(layoutInflater)
+
+    private lateinit var mBtnWelcome: CustomAuthenticationButton
+
+    override fun onBinding() {
+        initViews()
+        setUpClickListeners()
+    }
+
+    private fun initViews() {
+        mViewBinding.run {
+            mBtnWelcome = btnWelcome
+        }
+    }
+
+    private fun setUpClickListeners() {
+        mBtnWelcome.setOnClickListener {
+            activity?.finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/gthr/gthrcollect/ui/welcome/WelcomeViewModel.kt
@@ -0,0 +1,5 @@
+package com.gthr.gthrcollect.ui.welcome
+
+import com.gthr.gthrcollect.ui.base.BaseViewModel
+
+class WelcomeViewModel : BaseViewModel() {}
