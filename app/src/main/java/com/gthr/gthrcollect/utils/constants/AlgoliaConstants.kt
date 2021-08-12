87e9a88f7a4ebbd023f728d77fe72c2a5462f177
Deadshot <pankaj.makhija26@gmail.com>
2021-08-12 17:55:34 +0530

    [GA-212] Update algolia Api key

--- app/src/main/java/com/gthr/gthrcollect/utils/constants/AlgoliaConstants.kt
+++ app/src/main/java/com/gthr/gthrcollect/utils/constants/AlgoliaConstants.kt
@@ -2,7 +2,7 @@ package com.gthr.gthrcollect.utils.constants
 
 object AlgoliaConstants {
     const val APP_ID = "0BGN8TTV9B"
-    const val APIKey="aec05eebbdd114cbdd40a3b74f683c87"
+    const val APIKey=/*"aec05eebbdd114cbdd40a3b74f683c87"*/"f596ab9336a831cadd703a08764edf09"
     const val COLLECTION_INFO_MODEL="AlgoliaCollectionModel"
 
     /* AlgoliaCollectionModel */
