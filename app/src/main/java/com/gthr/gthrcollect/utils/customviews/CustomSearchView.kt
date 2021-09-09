f11f36d753fa7db91b901490121cafc52ea7de38
Pankaj Makhija <pankaj.makhija26@gmail.com>
2021-09-09 20:56:25 +0530

    [GA-307] Search - Bar

--- app/src/main/java/com/gthr/gthrcollect/utils/customviews/CustomSearchView.kt
+++ app/src/main/java/com/gthr/gthrcollect/utils/customviews/CustomSearchView.kt
@@ -2,12 +2,15 @@ package com.gthr.gthrcollect.utils.customviews
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import androidx.appcompat.widget.AppCompatEditText
 import com.gthr.gthrcollect.R
 import com.gthr.gthrcollect.utils.extensions.afterTextChanged
 import com.gthr.gthrcollect.utils.extensions.getImageDrawable
 import com.gthr.gthrcollect.utils.extensions.getResolvedColor
+
 
 class CustomSearchView @JvmOverloads constructor(
     context: Context,
@@ -15,7 +18,9 @@ class CustomSearchView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
 
-    init{
+    private var isEmpty = false
+
+    init {
         setSearchBackground(true)
         this.isFocusable = true
         this.isFocusableInTouchMode = true
@@ -27,14 +32,19 @@ class CustomSearchView @JvmOverloads constructor(
         this.compoundDrawablePadding = paddingVertical
 
         setTextChangeListener {}
+        onCancelListener()
     }
 
-    fun setSearchBackground(isEmpty: Boolean) {
-        if (isEmpty) {
+    private fun setSearchBackground(isEmpty: Boolean) {
+        if (this.isEmpty == isEmpty)
+            return
+
+        this.isEmpty = isEmpty
+        if (this.isEmpty) {
             this.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_search, 0, 0, 0)
             this.background = getImageDrawable(R.drawable.bg_search_edit_text_gray)
         } else {
-            this.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+            this.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_round_close, 0)
             this.background = getImageDrawable(R.drawable.bg_search_edit_text_blue)
         }
     }
@@ -48,5 +58,26 @@ class CustomSearchView @JvmOverloads constructor(
             }
             search(it)
         }
+    }
+
+    private fun onCancelListener() {
+        this.setOnTouchListener(OnTouchListener { v, event ->
+            try {
+
+//            val DRAWABLE_LEFT = 0
+//            val DRAWABLE_TOP = 1
+                val DRAWABLE_RIGHT = 2
+//            val DRAWABLE_BOTTOM = 3
+
+                if (event.action == MotionEvent.ACTION_UP)
+                    if (event.rawX >= this.right - this.compoundDrawables[DRAWABLE_RIGHT].bounds.width()) {
+                        this.setText("")
+                        return@OnTouchListener true
+                    }
+                false
+            } catch (e: Exception) {
+                false
+            }
+        })
     }
 }
